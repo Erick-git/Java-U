71768a1c7bacb8ebc82b82c95d5cf5c357fd549d
Erick gitPrueba1 <erick666666@gmail.com>
2024-07-31 22:24:10 -0600

    Agregando animal al zoologico

---
+++ Zoo/Zoo/src/Animal.java
@@ -0,0 +1,38 @@
+public class Animal {
+    String name;
+    int age;
+    String gender;
+    int pound;
+    String color;
+ 
+    public Animal(String name, int age, String sexo, int pound, String color) {
+        
+        this.name = name;
+        this.age = age;
+        this.gender = sexo;
+        this.pound = pound;
+        this.color = color;
+    }
+
+    public void speak() {
+      
+        System.out.println("Mi nombre es " + name);
+        System.out.println("Tengo " + age + " años de edad");
+        System.out.println("Soy " + gender);
+        System.out.println("Peso aprox " + pound + " libras");
+        System.out.println("Soy de color " + color);
+    }
+
+    public void walk(){
+        System.out.println("Corro en la selva");
+    }
+
+    public void eat(){
+        System.out.println("Desayuno humanos");
+    }
+
+    public void sleep(){
+        System.out.println("Duermo en el día");
+    }
+}
+
---
+++ Zoo/Zoo/src/Zoo.java
@@ -0,0 +1,16 @@
+public class Zoo {
+    public static void main(String[] args) {
+        
+        
+       Animal animalito1 = new Animal("Simba", 10, "Macho", 50 , "Amarillo");
+ 
+        animalito1.speak();
+        animalito1.eat();
+        animalito1.sleep();
+        animalito1.walk();
+    
+     }
+
+  
+ }  
+
